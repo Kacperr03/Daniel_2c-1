1f30fd46656fb30aac4cb9c5ef17d7d0408f8b6b
Kacperr03 <kacperd258@gmail.com>
2021-05-24 09:21:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pierwszy skrypt php</title>
+    </head>
+    <body>
+        <?php 
+        // Skrypt ten wypisze tekst, używając funkcji 'print'
+        print("PHP");
+        ?>
+    </body>
+</html>
